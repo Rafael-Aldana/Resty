a0fcfccb0bbcfc89b0a757fd1fe395b2102a0b87
Rafael Aldana <rafaelaldana6@Gmail.com>
2023-03-23 09:29:03 -0700

    use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState} from 'react';
 
 import './App.scss';
 
@@ -10,6 +10,7 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Form from './Components/Form';
 import Results from './Components/Results';
+import axios from 'axios';
 // const [something, setSomthing] = useState('something');
 // const [age, setAge] = useState(0);
 // const [factorOfFive, setFactorOfFive] = useState(false);
@@ -25,23 +26,25 @@ const [loading, setLoading] = useState(false);
   //     requestParams: {},
   //   };
   // }
-
- const callApi = (requestParams) => {
+  
+  const callApi = (requestParams) => {
     // mock output
     setLoading(true);
     setTimeout(() =>{
-      const data = {
-        count: 2,
-        results: [
-          {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-          {name: 'fake thing 2', url: 'http://fakethings.com/2'},
-        ],
-      };
-      setData(data);
       setRequestParams(requestParams);
       setLoading(false);
     }, 3000)
+  }
+  
+  useEffect(() => {
+    const getData = async () => {
+    console.log('RequestParams have Changed')
+    let response = await axios.get(requestParams);
+    setData(response.data);
     }
+    getData();
+  }, [requestParams]);
+  
     return (
       <React.Fragment>
         <Header />
